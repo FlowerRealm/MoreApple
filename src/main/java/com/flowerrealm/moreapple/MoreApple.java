17642db90381be522d210231725fbd1274fd86b3
FlowerRealm <flower_realm@outlook.com>
2026-05-29 19:03:51 +0800

    Remove survival access to golden apples

--- src/main/java/com/flowerrealm/moreapple/MoreApple.java
+++ src/main/java/com/flowerrealm/moreapple/MoreApple.java
@@ -1,6 +1,10 @@
 package com.flowerrealm.moreapple;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,6 +14,22 @@ public final class MoreApple implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        registerBuiltinDataPacks();
         LOGGER.info("More Apple initialized");
+    }
+
+    private static void registerBuiltinDataPacks() {
+        var container = FabricLoader.getInstance().getModContainer(MOD_ID)
+            .orElseThrow(() -> new IllegalStateException(MOD_ID + " mod container is missing"));
+
+        boolean registered = ResourceManagerHelper.registerBuiltinResourcePack(
+            new Identifier(MOD_ID, "tconstruct_overrides"),
+            container,
+            ResourcePackActivationType.ALWAYS_ENABLED
+        );
+
+        if (!registered) {
+            throw new IllegalStateException("Failed to register More Apple TConstruct data overrides");
+        }
     }
 }
